7bbc10b4a2e6185943ce14fa24428711c5a95cd0
AnthonyHalys <66371510+AnthonyHalys@users.noreply.github.com>
2023-01-18 14:46:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ if streamlit.button('Get Fruit Load List'):
       streamlit.dataframe(my_data_rows) 
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+# streamlit.stop()
 
 #Permettre l'ajout d'un fruit à la liste ; réponse kiwi par défaut
 # Fonction pour l'ajout
