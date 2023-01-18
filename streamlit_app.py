212aed4849f0ef4620aca8ef244797ef3f784534
AnthonyHalys <66371510+AnthonyHalys@users.noreply.github.com>
2023-01-18 11:30:36 +0100

    Update streamlit_app.py
    Ajout de la possibilité d'ajouter un fruit à la liste

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,21 +43,21 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-# requete sur les metadata ajoutées
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+## requete sur les metadata ajoutées
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
 
-# requete sur les données
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains :")
-#streamlit.text(my_data_row)
-streamlit.dataframe(my_data_row)
+## requete sur les données
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("The fruit load list contains :")
+# #streamlit.text(my_data_row)
+# streamlit.dataframe(my_data_row)
 
 # requete sur TOUTES les données(as juste une ligne
 my_cur = my_cnx.cursor()
@@ -66,3 +66,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains :")
 #streamlit.text(my_data_row)
 streamlit.dataframe(my_data_rows) 
+
+#Permettre l'ajout d'un fruit à la liste ; réponse kiwi par défaut
+add_fruit = streamlit.text_input('What fruit would you to add')
+streamlit.write('Thanks for adding ', add_fruit)
